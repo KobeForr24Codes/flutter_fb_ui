b22ea17a6364c28eb4e5aac057c5f62513dd3083
KobeForr24Codes <kbpforrosuelo@dmc.edu.ph>
2020-10-08 08:34:43 +0800

    ready for state integration

--- lib/widgets/profile_avatar.dart
+++ lib/widgets/profile_avatar.dart
@@ -1,7 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import '../config/palette.dart';
-import '../config/palette.dart';
 
 class ProfileAvatar extends StatelessWidget {
   final String imageUrl;
